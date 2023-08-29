5e889b17f8712a05946033ea0764c3eb744f2150
Ana Juárez <lisa13suarez@gmail.com>
2023-08-28 22:23:57 -0600

    Añadiendo file

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import foto from './assets/Group 1000000929.png'
+function App() {
+
+
+  return (
+    <>
+      <div className='marco'>
+        <div className='foto'>
+          <img src={foto} alt="" />
+        </div>
+        <input type="text" placeholder='Escribe algo!' />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, nisi totam. Rem odio laudantium, dicta tenetur quos quas rerum ullam voluptatibus eius quibusdam. Quasi soluta nulla quia, quidem corrupti porro!
+      Ducimus maiores ab debitis enim, ipsam tempore quam facilis. Maxime praesentium repellendus itaque, fugit vel neque ipsam culpa voluptatum qui dolorum quas aliquid voluptatibus doloribus tenetur ex eaque repudiandae sequi!
+      Accusantium minus soluta enim dolore iure. Vitae deleniti reprehenderit facere nisi numquam est possimus minima laborum pariatur quisquam autem delectus suscipit molestiae veritatis dignissimos, magni fugiat, nam, voluptas alias eligendi!
+      Excepturi at fugiat mollitia nobis rem exercitationem ex error velit sequi fugit eos ullam laudantium maiores libero rerum, porro dicta qui sed est! Nesciunt consectetur maxime dicta commodi atque quos!
+      Quasi, magni cupiditate corrupti laboriosam, iusto ad nesciunt similique ratione quam labore quod est ullam sapiente commodi tempore soluta facilis ipsam tempora dolores quaerat quia, facere consequuntur minima odit! Aspernatur.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
